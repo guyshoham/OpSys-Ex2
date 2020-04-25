d7bb524ac330518653c6164df33b84c41bdea7c0
Guy Shoham <guyshoham28@gmail.com>
2020-04-25 19:01:16 +0300

    split to functions

--- main.c
+++ main.c
@@ -3,63 +3,39 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+
+bool isBackground();
+void backgroundCommand();
+void foregroundCommand();
+void splitCommand();
+
+char* argv[10];
+int argc;
+char command[100];
 
 int main() {
-  int status;
-  char command[100];
-  char* argv[10];
-  pid_t val;
-  int stat;
 
   ///scan command input
   printf("> ");
   gets(command);
   while (strcmp(command, "exit")) {
 
-    // split the input
-    char* token = strtok(command, " ");
-    int argc = 0;
+    if (strlen(command) > 0) {
 
-    while (token != NULL) {
-      argv[argc] = token;
-      argc++;
-      printf("%s\n", token);
-      token = strtok(NULL, " ");
-    }
-    argv[argc] = NULL;
+      // split the input
+      splitCommand();
 
-    //make the command (foreground or background)
-    if (strcmp(argv[argc - 1], "&") == 0) {
-      // if background, remove '&' from argv
-      argv[argc - 1] = NULL;
-      printf("Background\n");
-
-      //forking, child process will execvp, parent won't wait
-      val = fork();
-      if (val == 0) { ///child process
-        printf("%d\n", (int) getpid());
-        status = execvp(argv[0], argv);
-        if (status == -1) {
-          printf("exec failed\n");
-        }
-        return 0;
+      //make the command (foreground or background)
+      if (isBackground()) {
+        // if background, remove '&' from argv
+        argv[argc - 1] = NULL;
+        backgroundCommand(argv);
+      } else {
+        foregroundCommand(argv);
       }
-    } else {
-      printf("Foreground\n");
 
-      //forking, child process will execvp and parent will wait
-      val = fork();
-      if (val == 0) { ///child process
-        printf("%d\n", (int) getpid());
-        status = execvp(argv[0], argv);
-        if (status == -1) {
-          printf("exec failed\n");
-        }
-        return 0;
-      } else if (val > 0) { ///parent process
-        wait(&stat);
-      }
-    }
+    }// end of empty string check
 
     ///scan another command input
     printf("> ");
@@ -68,3 +44,53 @@ int main() {
 
   return 0;
 }
+void splitCommand() {
+  argc = 0;
+  char* token = strtok(command, " ");
+
+  while (token != NULL) {
+    argv[argc] = token;
+    argc++;
+    token = strtok(NULL, " ");
+  }
+  argv[argc] = NULL;
+}
+void foregroundCommand() {
+  pid_t val;
+  int stat;
+  int status;
+
+  printf("Foreground\n"); //TODO: delete line before submit
+
+  //forking, child process will execvp and parent will wait
+  val = fork();
+  if (val == 0) { ///child process
+    printf("%d\n", (int) getpid());
+    status = execvp(argv[0], argv);
+    if (status == -1) {
+      printf("exec failed\n");
+    }
+  } else if (val > 0) { ///parent process
+    wait(&stat);
+  }
+}
+void backgroundCommand() {
+  pid_t val;
+  int status;
+
+  printf("Background\n"); //TODO: delete line before submit
+
+  //forking, child process will execvp, parent won't wait
+  val = fork();
+  if (val == 0) { ///child process
+    printf("%d\n", (int) getpid());
+    status = execvp(argv[0], argv);
+    if (status == -1) {
+      printf("exec failed\n");
+    }
+  }
+}
+bool isBackground() {
+  return strcmp(argv[argc - 1], "&") == 0 ? true : false;
+}
+
